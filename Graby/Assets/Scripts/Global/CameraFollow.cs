7a7b4a24b378823634fce9504b11dd2f3f914c00
Slammus <sumrandomguy2003@hotmail.com>
2015-03-25 20:44:41 +1300

    Setting up basic controls and 4 players (requires controller at the moment).
    - Can move left and right with the left joystick.
    - Can jump with A.
    - Some basic set up for multiple players.

---
+++ Graby/Assets/Scripts/Global/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public GameObject m_ObjectToFollow;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_ObjectToFollow)
+		{
+			Transform camera = GetComponent<Transform>();
+			Vector3 currentPos = camera.position;
+			currentPos.x = m_ObjectToFollow.transform.position.x;
+			currentPos.y = m_ObjectToFollow.transform.position.y;
+			camera.position = currentPos;
+		}
+	}
+}
---
+++ Graby/Assets/Scripts/Global/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float m_Deadzone = 0.3f;
+	public uint m_PlayerNumber = 1;
+	public float m_MoveForce = 2500.0f;
+	public float m_JumpForce = 10000.0f;
+	
+	private Rigidbody m_rb;
+	private LineRenderer m_lr;
+
+	void Start ()
+	{
+		m_rb = gameObject.GetComponent<Rigidbody> ();
+		m_lr = gameObject.AddComponent<LineRenderer> ();
+		m_lr.material = new Material(Shader.Find("Particles/Additve"));
+		m_lr.SetColors (Color.yellow, Color.red);
+		m_lr.SetWidth (0.1f, 0.1f);
+		m_lr.SetVertexCount (2);
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 movementForce = Vector3.zero;
+		float joystickX = GetJoystickX ();
+		if (System.Math.Abs (joystickX) > Mathf.Epsilon) 
+		{
+			movementForce.x = joystickX * m_MoveForce * Time.fixedDeltaTime;
+		}
+
+		if (Input.GetButton ("A_" + m_PlayerNumber) && IsGrounded ()) 
+		{
+			m_rb.velocity = new Vector3 (m_rb.velocity.x, 0.0f, m_rb.velocity.z);
+			movementForce.y = m_JumpForce;
+		}
+
+		m_rb.AddForce(movementForce);
+	}
+	
+	bool IsGrounded()
+	{
+		float colliderHeight = gameObject.GetComponent<Collider> ().bounds.extents.y;
+		bool isOnGround = Physics.Raycast (transform.position, -Vector3.up, colliderHeight + 0.1f);
+		bool movingUp = m_rb.velocity.y > Mathf.Epsilon;
+		return isOnGround && !movingUp;
+	}
+
+	float GetJoystickX()
+	{
+		float xInputRaw = Input.GetAxis ("L_XAxis_" + m_PlayerNumber);
+		if(System.Math.Abs(xInputRaw) <= m_Deadzone)
+		{
+			return 0.0f;
+		}
+		else if(xInputRaw < 0.0f)
+		{
+			return (xInputRaw + m_Deadzone) / (1.0f - m_Deadzone);
+		}
+		else
+		{
+			return (xInputRaw - m_Deadzone) / (1.0f - m_Deadzone);
+		}
+	}
+}
