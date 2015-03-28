0ea51adcef89a1bfaed0586cb19ef5047995d0b3
el-10dou <didier.pujol@gmail.com>
2015-03-29 00:49:15 +1300

    * GrabDirection : modifyed player to handle right stick for grab direction. print that in debug line renderer.

--- Graby/Assets/Scripts/Global/PlayerController.cs
+++ Graby/Assets/Scripts/Global/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 	public uint m_PlayerNumber = 1;
 	public float m_MoveForce = 2500.0f;
 	public float m_JumpForce = 10000.0f;
+	public Vector3 m_GrabDir = Vector3.zero;
 	
 	private Rigidbody m_rb;
 	private LineRenderer m_lr;
@@ -16,11 +17,32 @@ public class PlayerController : MonoBehaviour
 		m_rb = gameObject.GetComponent<Rigidbody> ();
 		m_lr = gameObject.AddComponent<LineRenderer> ();
 		m_lr.material = new Material(Shader.Find("Particles/Additve"));
-		m_lr.SetColors (Color.yellow, Color.red);
+		m_lr.SetColors (Color.yellow, Color.blue);
 		m_lr.SetWidth (0.1f, 0.1f);
 		m_lr.SetVertexCount (2);
 	}
 
+	//-------------------------------------//
+	// Update()
+	//
+	//  Update is called every frame
+	//-------------------------------------//
+	void Update()
+	{
+		GetGrabDirection( ref m_GrabDir );
+
+		// DEBUG DRAW
+	    m_lr.SetPosition(0, this.transform.position );
+		m_lr.SetPosition(1, this.transform.position + m_GrabDir * 10.0f );
+	}
+
+	//-------------------------------------//
+	// FixedUpdate()
+	//
+	// This function is called every fixed framerate frame
+	// FixedUpdate should be used instead of Update when dealing with Rigidbody. 
+	// For example when adding a force to a rigidbody, you have to apply the force every fixed frame inside FixedUpdate instead of every frame inside Update.
+	//-------------------------------------//
 	void FixedUpdate ()
 	{
 		Vector3 movementForce = Vector3.zero;
@@ -49,7 +71,7 @@ public class PlayerController : MonoBehaviour
 
 	float GetJoystickX()
 	{
-		float xInputRaw = Input.GetAxis ("L_XAxis_" + m_PlayerNumber);
+		float xInputRaw = Input.GetAxisRaw ("L_XAxis_" + m_PlayerNumber);
 		if(System.Math.Abs(xInputRaw) <= m_Deadzone)
 		{
 			return 0.0f;
@@ -61,6 +83,23 @@ public class PlayerController : MonoBehaviour
 		else
 		{
 			return (xInputRaw - m_Deadzone) / (1.0f - m_Deadzone);
+		}
+	}
+
+	// GetGrabDirection(ref Vector2 outVector )
+	//
+	// Compute a 3D vectore based on the right analog stick
+	// and put it in outVector
+	void GetGrabDirection(ref Vector3 outVector )
+	{
+		float xInputRaw = Input.GetAxisRaw ("R_XAxis_" + m_PlayerNumber);
+		float yInputRaw = Input.GetAxisRaw ("R_YAxis_" + m_PlayerNumber);
+		if(System.Math.Abs( xInputRaw ) + System.Math.Abs( yInputRaw ) > m_Deadzone )
+		{
+			outVector.x = xInputRaw;
+			outVector.y = -yInputRaw;
+			outVector.z = 0.0f;
+			outVector.Normalize();
 		}
 	}
 }
